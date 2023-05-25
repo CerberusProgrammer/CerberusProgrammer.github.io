f2c1e6293e57688cbc2ec064a96229da4c6d1987
sazarcode <cerberusprogrammer@gmail.com>
2023-05-24 20:23:26 -0700

    Future implemention for personal web.

---
+++ cerberusprogrammer/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:cerberusprogrammer/themes.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'home.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final savedThemeMode = await AdaptiveTheme.getThemeMode();
+  final prefs = await SharedPreferences.getInstance();
+
+  Themes.defaultIndex = prefs.getInt('defaultIndex') ?? 10;
+  bool presentation = prefs.getBool('presentation') ?? true;
+
+  runApp(AdaptiveTheme(
+      light: ThemeData(
+        brightness: Brightness.light,
+        colorSchemeSeed: Themes.colors[Themes.defaultIndex],
+        useMaterial3: true,
+      ),
+      dark: ThemeData(
+        brightness: Brightness.dark,
+        colorSchemeSeed: Themes.colors[Themes.defaultIndex],
+        useMaterial3: true,
+      ),
+      initial: savedThemeMode ?? AdaptiveThemeMode.light,
+      builder: (theme, darkTheme) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: "Enfo",
+          theme: theme,
+          darkTheme: darkTheme,
+          home: const Home(),
+        );
+      }));
+}
