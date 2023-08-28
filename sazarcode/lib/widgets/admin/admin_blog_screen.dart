e29bebddaed8fb1c18efb38b4340bf2187cf16d9
sazarcode <cerberusprogrammer@gmail.com>
2023-08-27 17:59:42 -0700

    New supereditor integrated.

--- sazarcode/lib/widgets/admin/admin_blog_screen.dart
+++ sazarcode/lib/widgets/admin/admin_blog_screen.dart
@@ -1,27 +1,47 @@
+import 'package:appflowy_editor/appflowy_editor.dart';
 import 'package:flutter/material.dart';
-import 'package:super_editor/super_editor.dart';
 
 class AdminBlogScreen extends StatelessWidget {
   const AdminBlogScreen({super.key});
 
+  EditorStyle customizeEditorStyle() {
+    return const EditorStyle.mobile().copyWith(
+      cursorColor: Colors.green,
+      selectionColor: Colors.green[100],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final document = MutableDocument(
-      nodes: [
-        ParagraphNode(
-          id: DocumentEditor.createNodeId(),
-          text: AttributedText(text: 'This is some text.'),
-        ),
+    final editorState = EditorState.blank(withInitialText: true);
+
+    final editor = AppFlowyEditor(
+      editorState: editorState,
+      contextMenuItems: [
+        [
+          ContextMenuItem(
+            name: 'name',
+            onPressed: (editorState) {},
+          )
+        ]
       ],
     );
-    final editor = DocumentEditor(document: document);
 
     return MaterialApp(
-      home: Scaffold(
-        body: SuperEditor(
-          editor: editor,
-        ),
+      localizationsDelegates: const [
+        AppFlowyEditorLocalizations.delegate,
+      ],
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.green,
       ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Text Editor'),
+          ),
+          body: Center(
+            child: editor,
+          )),
     );
   }
 }
